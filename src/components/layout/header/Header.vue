22fc9d7c4dc641f8eb54720aa13d8e7d28afd5f0
Samuel <s.mendezgarcia@student.helmo.be>
2024-10-17 22:55:10 +0200

    Implemented responsive navigation bar and layout components

--- src/components/layout/header/Header.vue
+++ src/components/layout/header/Header.vue
@@ -1,24 +1,49 @@
 <template>
   <header class="flex justify-center items-center px-4 py-2 bg-custom-background-dark text-custom-white">
-    <div class="w-3/5 flex justify-between items-center">
+    <div class="w-full md:w-3/5 flex justify-between items-center">
+
+      <!-- logo + links(if not mobile) -->
       <div class="flex items-center">
         <a href="/" class="mr-5">
           <img src="@/assets/logoBlue.png" alt="Logo" class="h-6">
         </a>
-        <NavLinks />
+        <NavLinks class="hidden md:flex" />
       </div>
-      <div class="flex items-center space-x-2">
+
+      <!-- profile et alerts(if not mobile) -->
+      <div class="hidden md:flex items-center justify-end space-x-4">
         <ProfileMenu />
         <AlertMenu />
       </div>
+
+      <!-- if mobile, display a burger -->
+      <div class="md:hidden">
+        <button @click="toggleMenu" class="focus:outline-none">
+          <BurgerMenu/>
+        </button>
+      </div>
+    </div>
+
+    <!-- if the menu is open (burger button cliqued) on affiche le composant -->
+    <div v-if="isMenuOpen" class="absolute right-0 top-16 w-48 bg-custom-background-dark text-white rounded-lg shadow-lg p-4 md:hidden z-50">
+      <MobileMenu />
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import NavLinks from '@/components/layout/header/NavLinks.vue'
 import ProfileMenu from '@/components/layout/header/ProfileMenu.vue'
 import AlertMenu from '@/components/layout/header/AlertMenu.vue'
+import BurgerMenu from '@/components/layout/header/BurgerMenu.vue'
+import MobileMenu from '@/components/layout/header/MobileMenu.vue'
+
+const isMenuOpen = ref(false)
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value
+}
 </script>
 
 <style scoped></style>
