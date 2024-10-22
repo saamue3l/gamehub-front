1212cf614d3e81fd7b29e6fa55705d1f036959d3
Guillitte Matthias <m.guillitte@student.helmo.be>
2024-10-22 13:36:38 +0200

    Composants shadcdn

--- src/components/ui/inputs/datePicker/DatePicker.vue
+++ src/components/ui/inputs/datePicker/DatePicker.vue
@@ -1,3 +1,4 @@
+<!--
 <script setup lang="ts"></script>
 
 <template>
@@ -8,3 +9,40 @@
 </template>
 
 <style scoped></style>
+-->
+
+<script setup lang="ts">
+import { Button } from '@/components/ui/button/Button.vue'
+import { Calendar } from '@/components/ui/calendar'
+
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { cn } from '@/utils'
+import { DateFormatter, type DateValue, getLocalTimeZone } from '@internationalized/date'
+import { Calendar as CalendarIcon } from 'lucide-vue-next'
+import { ref } from 'vue'
+
+const df = new DateFormatter('en-US', {
+  dateStyle: 'long'
+})
+
+const value = ref<DateValue>()
+</script>
+
+<template>
+  <Popover>
+    <PopoverTrigger as-child>
+      <Button
+        variant="outline"
+        :class="
+          cn('w-[280px] justify-start text-left font-normal', !value && 'text-muted-foreground')
+        "
+      >
+        <CalendarIcon class="mr-2 h-4 w-4" />
+        {{ value ? df.format(value.toDate(getLocalTimeZone())) : 'Pick a date' }}
+      </Button>
+    </PopoverTrigger>
+    <PopoverContent class="w-auto p-0">
+      <Calendar v-model="value" initial-focus />
+    </PopoverContent>
+  </Popover>
+</template>
