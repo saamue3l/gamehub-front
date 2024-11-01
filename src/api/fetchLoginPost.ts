f8fb8149877d3576a5036cf0757fb86223c00c3b
Mendez Garcia Samuel <s.mendezgarcia@student.helmo.be>
2024-11-01 09:31:08 +0000

    Gestion messages erreur

--- src/api/fetchLoginPost.ts
+++ src/api/fetchLoginPost.ts
@@ -1,34 +1,24 @@
 import { getCsrfCookie } from '@/api/getCsrfCookie'
 import type { LoginPost, LoginResponse } from '@/types/Login'
+import { httpBackend } from '@/lib/utils'
 
 export async function fetchLoginPost(values: LoginPost): Promise<LoginResponse> {
   await getCsrfCookie()
-  return fetch(`${import.meta.env.VITE_BACKEND_HOST}/api/login`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      'X-Requested-With': 'XMLHttpRequest'
-    },
-    body: JSON.stringify(values),
-    credentials: 'include'
-  }).then(async (response) => {
-    if (response.ok) {
-      const data = await response.json()
-      storeSessionData(data)
-      return data
-    } else {
-      const errorResponse = response.json()
-      throw new Error(errorResponse.message || 'Échec de la connexion.')
-    }
-  })
+
+  try {
+    const data = await httpBackend<LoginResponse>('/api/login', 'POST', values, false)
+    storeSessionData(data)
+    return data
+  } catch (error) {
+    throw new Error(error instanceof Error ? error.message : 'Échec de la connexion.')
+  }
 }
 
-function storeSessionData(data) {
+function storeSessionData(data: LoginResponse) {
   sessionStorage.setItem('token', data.token)
   sessionStorage.setItem('username', data.user.username)
-  sessionStorage.setItem('roleId', data.user.roleId)
-  sessionStorage.setItem('statusId', data.user.statusId)
-  sessionStorage.setItem('xp', data.user.xp)
+  sessionStorage.setItem('roleId', data.user.roleId.toString())
+  sessionStorage.setItem('statusId', data.user.statusId.toString())
+  sessionStorage.setItem('xp', data.user.xp.toString())
   sessionStorage.setItem('picture', data.user.picture)
 }
