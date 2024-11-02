da73e1c102161015165348091b4358a551351066
Montesanti Raphaël <r.montesanti@student.helmo.be>
2024-11-02 15:12:50 +0000

    First commit for front...

--- src/router/index.js.ts
+++ src/router/index.js.ts
@@ -5,6 +5,7 @@ import Profile from '@/views/Profile.vue'
 import type { Component } from 'vue'
 import Login from '@/components/home/Login.vue'
 import Register from '@/components/home/Register.vue'
+import Matchmaking from '@/views/Matchmaking.vue'
 
 type Route = {
   path: string
@@ -18,6 +19,7 @@ const routes: Route[] = [
   { path: '/events', name: 'Évènements', component: Events, inNav: true },
   { path: '/joachim', name: 'Love', component: () => import('@/views/Joachim.vue') },
   { path: '/profil/:username', name: 'Profil', component: Profile },
+  { path: '/matchmaking', name: 'Matchmaking', component: Matchmaking, inNav: true},
   { path: '/login', name: 'Connexion', component: Login },
   { path: '/register', name: 'Register', component: Register }
 ]
