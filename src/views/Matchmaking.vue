da458576d8ee77c802b3c12a6897fcde1ef3e183
Raphael Montesanti <r.montesanti@student.helmo.be>
2024-11-02 15:41:07 +0100

    Last updates for more coherence

--- src/views/Matchmaking.vue
+++ src/views/Matchmaking.vue
@@ -1,28 +1,17 @@
 <script setup lang="ts">
 import BasePage from '@/components/layout/BasePage.vue';
 import SearchGame from '@/components/layout/games/SearchGame.vue';
-import { ref, onMounted } from 'vue';
-import { Popover, PopoverTrigger, PopoverContent } from '@/components/ui/popover';
+import SkillsCombobox from '@/components/layout/skills/SkillsCombobox.vue';
+import { ref, watch } from 'vue';
 import { Dialog, DialogTrigger, DialogTitle, DialogDescription, DialogFooter, DialogHeader, DialogScrollContent } from '@/components/ui/dialog';
-import { Command, CommandGroup, CommandItem, CommandList } from '@/components/ui/command';
-import { Check, ChevronsUpDown } from 'lucide-vue-next';
-import { cn, httpBackend } from '@/lib/utils'
+import { httpBackend } from '@/lib/utils'
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Button } from '@/components/ui/button'
-
-
-
-const niveaux = [
-  { value: '1', label: 'Débutant' },
-  { value: '2', label: 'Intermédiaire' },
-  { value: '3', label: 'Avancé' },
-]
 
 const isDialogOpen = ref(false);
 const selectedGames = ref([]);
 const selectedGame = ref<any>(null);
-const selectedLevel = ref('');
-const open = ref(false);
+const selectedLevel = ref(null);
 const gameError = ref(false);
 const levelError = ref(false);
 const isLoading = ref(false);
@@ -31,10 +20,15 @@ const matchResults = ref<any[]>([]);
 
 function handleSelectGame(game: any) {
   selectedGame.value = game;
-  gameError.value = !game;
+  gameError.value = false;
 }
 
-function confirmAddGame() {
+function handleSelectLevel(level: any) {
+  selectedLevel.value = level;
+  levelError.value = false;
+}
+
+function handleSubmit() {
   // Validate game and level selection
   gameError.value = !selectedGame.value;
   levelError.value = !selectedLevel.value;
@@ -42,21 +36,32 @@ function confirmAddGame() {
   if (selectedGame.value && selectedLevel.value) {
     addGame();
     isDialogOpen.value = false;
-    selectedGame.value = null;
-    selectedLevel.value = '';
+    resetDialog();
   }
 }
 
 function addGame() {
   if (selectedGame.value && selectedLevel.value) {
+
+    if (isGameAlreadyAdded(selectedGame.value, selectedLevel.value)) {
+      errorMessage.value = "Ce jeu avec ce niveau est déjà ajouté.";
+      return;
+    }
+
     const gameToAdd = {
       ...selectedGame.value,
-      level: selectedLevel.value,
-      levelLabel: niveaux.find(n => n.value === selectedLevel.value)?.label || '',
+      level: selectedLevel.value.id,
+      levelLabel: selectedLevel.value.label,
     };
     selectedGames.value.push(gameToAdd);
     sendGamesToApi();
   }
+}
+
+function isGameAlreadyAdded(game: any, level: any) {
+  return selectedGames.value.some(
+    (selectedGame) => selectedGame.id === game.id && selectedGame.level === level.id
+  );
 }
 
 function removeGame(index: number) {
@@ -64,18 +69,16 @@ function removeGame(index: number) {
   sendGamesToApi();
 }
 
-function selectLevel(level: string) {
-  selectedLevel.value = level;
-  levelError.value = false;
-  open.value = false;
-}
-
 function closeDialog() {
   isDialogOpen.value = false;
-  levelError.value = false;
-  gameError.value = false;
+  resetDialog();
+}
+
+function resetDialog() {
   selectedGame.value = null;
-  selectedLevel.value = '';
+  selectedLevel.value = null;
+  gameError.value = false;
+  levelError.value = false;
 }
 
 async function sendGamesToApi() {
@@ -90,7 +93,6 @@ async function sendGamesToApi() {
       gameId: game.id,
       skillTypeId: game.level,
     }));
-    console.log(gamesData);
     const response = await httpBackend('/api/matchmaking', 'POST', gamesData);
 
     if (response.status !== 'success') {
@@ -104,6 +106,12 @@ async function sendGamesToApi() {
     isLoading.value = false;
   }
 }
+
+watch(isDialogOpen, (newVal) => {
+  if (!newVal) {
+    resetDialog();
+  }
+});
 </script>
 
 <template>
@@ -116,14 +124,12 @@ async function sendGamesToApi() {
         <div
           v-for="(game, index) in selectedGames"
           :key="index"
-          class="relative border border-white rounded-full mt-2 flex flex-row items-center mr-2"
-          style="padding: 0.25rem; font-size: 0.75rem;"
-        >
+          class="relative border border-white rounded-full mt-2 flex flex-row items-center mr-2 p-1 text-sm"
+          style="padding: 0.25rem; font-size: 0.75rem;">
           <button
             @click="removeGame(index)"
-            class="relative text-white font-bold mx-1 enlarge-clickable-area"
-            style="font-size: 0.75rem; line-height: 1rem;"
-          >
+            class="relative text-white font-bold mx-1 enlarge-clickable-area text-sm leading-none"
+            style="font-size: 0.75rem; line-height: 1rem;">
             &times;
           </button>
           <div class="text-white font-bold mr-2">{{ game.name }}</div>
@@ -134,7 +140,7 @@ async function sendGamesToApi() {
         <Dialog v-model:open="isDialogOpen" @close="closeDialog">
           <DialogTrigger as-child>
             <div
-              class="relative border border-white rounded-full p-2 mt-2 flex flex-row items-center justify-center cursor-pointer bg-blue-600 text-white font-bold"
+              class="relative border border-white rounded-full p-2 mt-2 flex flex-row items-center justify-center cursor-pointer bg-blue-600 text-white font-bold text-sm"
               style="padding: 0.25rem; font-size: 0.75rem;"
             >
               <span class="mx-1">+</span>
@@ -142,75 +148,41 @@ async function sendGamesToApi() {
             </div>
           </DialogTrigger>
           <DialogScrollContent class="flex flex-col gap-6">
-            <DialogHeader>
-              <DialogTitle>Ajouter un jeu</DialogTitle>
-              <DialogDescription>Choisissez un jeu et votre niveau.</DialogDescription>
-            </DialogHeader>
+            <form @submit.prevent="handleSubmit">
+              <DialogHeader>
+                <DialogTitle>Ajouter un jeu</DialogTitle>
+                <DialogDescription>Choisissez un jeu et votre niveau.</DialogDescription>
+              </DialogHeader>
 
-            <!-- Recherche du jeu -->
-            <FormField name="gameId">
-              <FormItem>
-                <FormLabel>Jeu de l'évènement</FormLabel>
-                <FormControl>
-                  <SearchGame @select-game="handleSelectGame"></SearchGame>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </FormField>
+              <!-- Espace supplémentaire -->
+              <div class="mt-4"></div>
 
-            <!-- Message d'erreur jeu -->
-            <p v-if="gameError" class="text-red-500 text-sm">Veuillez sélectionner un jeu.</p>
+              <!-- Recherche du jeu -->
+              <FormField name="gameId">
+                <FormItem>
+                  <FormLabel :class="{ 'text-red-500': gameError }">Veuillez sélectionner un jeu.</FormLabel>
+                  <FormControl>
+                    <SearchGame @select-game="handleSelectGame"></SearchGame>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              </FormField>
 
-            <!-- Sélection du niveau -->
-            <Popover v-model:open="open">
-              <PopoverTrigger as-child>
-                <Button
-                  variant="outline"
-                  role="combobox"
-                  :aria-expanded="open"
-                  class="w-[200px] flex items-center justify-between"
-                >
-                  {{
-                    selectedLevel
-                      ? niveaux.find((niveau) => niveau.value === selectedLevel)?.label
-                      : "Votre niveau..."
-                  }}
-                  <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent class="w-[200px] p-0">
-                <Command>
-                  <CommandList>
-                    <CommandGroup>
-                      <CommandItem
-                        v-for="niveau in niveaux"
-                        :key="niveau.value"
-                        :value="niveau.value"
-                        @click="() => selectLevel(niveau.value)"
-                        class="cursor-pointer"
-                        :class="{
-                          'hover:bg-gray-300': true
-                        }"
-                      >
-                        {{ niveau.label }}
-                        <Check
-                          :class="cn(
-                            'ml-auto h-4 w-4',
-                            selectedLevel === niveau.value ? 'opacity-100' : 'opacity-0'
-                          )"
-                        />
-                      </CommandItem>
-                    </CommandGroup>
-                  </CommandList>
-                </Command>
-              </PopoverContent>
-            </Popover>
+              <!-- Sélection du niveau -->
+              <FormField name="levelId">
+                <FormItem class="flex flex-col">
+                  <FormLabel :class="{ 'text-red-500': levelError }">Veuillez sélectionner votre niveau.</FormLabel>
+                  <FormControl class="mt-2">
+                    <SkillsCombobox v-model="selectedLevel" @update:modelValue="handleSelectLevel" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              </FormField>
 
-            <!-- Message d'erreur niveau -->
-            <p v-if="levelError" class="text-red-500 text-sm">Veuillez sélectionner un niveau.</p>
-            <DialogFooter>
-              <Button type="button" @click="confirmAddGame">Ajouter</Button>
-            </DialogFooter>
+              <DialogFooter>
+                <Button type="submit">Ajouter</Button>
+              </DialogFooter>
+            </form>
           </DialogScrollContent>
         </Dialog>
       </div>
@@ -225,10 +197,10 @@ async function sendGamesToApi() {
           :to="`/profil/${result.username}`"
           class="profile-card border border-gray-300 rounded-lg p-4 m-2 text-center w-48"
         >
-          <img src="@/assets/joachim.jpg" alt="Jojo" class="profile-image mx-auto mb-2" />
-          <p class="username truncate">{{ result.username }}</p>
+          <img src="@/assets/joachim.jpg" alt="Jojo" class="profile-image mx-auto mb-2 rounded-full w-12 h-12" />
+          <p class="username truncate font-bold mb-2">{{ result.username }}</p>
           <p>Correspondance :</p>
-          <div class="progress-bar bg-gray-700 rounded-full h-2 mt-2">
+          <div class="progress-bar bg-gray-700 rounded-full h-2 mt-2 w-full">
             <div
               class="progress bg-blue-500 h-full rounded-full"
               :style="{ width: (result.gamesQtyFound / selectedGames.length) * 100 + '%' }"
@@ -241,44 +213,6 @@ async function sendGamesToApi() {
 </template>
 
 <style scoped>
-.profile-card {
-  width: 12rem; /* Adjust the width as needed */
-}
-
-.profile-image {
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-}
-
-.username {
-  font-weight: bold;
-  margin-bottom: 8px;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
-
-.progress-bar {
-  width: 100%;
-  background-color: #2d3748; /* Dark gray background */
-  border-radius: 4px;
-  overflow: hidden;
-  height: 0.5rem;
-  margin-top: 8px;
-}
-
-.progress {
-  height: 100%;
-  background-color: #4299e1; /* Blue color */
-}
-
-.results-container {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-}
-
 .enlarge-clickable-area::before {
   content: '';
   position: absolute;
