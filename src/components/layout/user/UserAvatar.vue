6cbe877b4d55969aec6f99af2de07d18406ef0e7
Guillitte Matthias <m.guillitte@student.helmo.be>
2024-11-26 09:56:47 +0100

    Moved the avatar into a separate component

---
+++ src/components/layout/user/UserAvatar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import type { User } from '@/types/User'
+import type { UserInfo } from '@/types/UserInfo'
+
+withDefaults(
+  defineProps<{
+    user: User | UserInfo
+    size: string
+  }>(),
+  {
+    size: '8'
+  }
+)
+</script>
+
+<template>
+  <Avatar :class="`size-${size}`">
+    <AvatarImage :src="user.picture" :alt="user.username" />
+    <AvatarFallback>{{ user.username.substring(0, 2).toUpperCase() }}</AvatarFallback>
+  </Avatar>
+</template>
+
+<style scoped></style>
